f9521add441ac626c8f66fb970528fb43f89ae78
jujuanma <juanmacueli@gmail.com>
2022-02-21 17:34:13 -0300

    inicio

---
+++ src/itemList.js
@@ -0,0 +1,24 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'PC Gamer',
+        price: 63999,
+        image: 'https://http2.mlstatic.com/D_NQ_NP_2X_878315-MLA48417394279_122021-F.webp',
+      },
+      {
+        id: '2',
+        name: 'Auricuralres',
+        price: 6719,
+        image: 'https://http2.mlstatic.com/D_NQ_NP_775551-MLA40154584077_122019-O.webp',
+      },
+      {
+        id: '3',
+        name: 'Mouse',
+        price: 3391,
+        image: 'https://http2.mlstatic.com/D_NQ_NP_867932-MLA44283118630_122020-O.webp',
+      },
+    ],
+  };
+  export default data;
+  
