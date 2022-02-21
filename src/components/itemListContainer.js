648fad3f4e1c74dbe3f227689ccf4bfefcc0750f
jujuanma <juanmacueli@gmail.com>
2022-02-21 18:47:39 -0300

    Update itemListContainer.js

--- src/components/itemListContainer.js
+++ src/components/itemListContainer.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-export default function ItemListContainer(props) {
-  const { ItemListContainer, onAdd } = props;
+export default function Product(props) {
+  const { product, onAdd } = props;
   return (
     <div>
-      <img className="small" src={ItemListContainer.image} alt={ItemListContainer.name} />
-      <h3>{ItemListContainer.name}</h3>
-      <div>${ItemListContainer.price}</div>
+      <img className="small" src={product.image} alt={product.name} />
+      <h3>{product.name}</h3>
+      <div>${product.price}</div>
       <div>
-        <button onClick={() => onAdd(ItemListContainer)}>Agregar al Carrito</button>
+        <button onClick={() => onAdd(product)}>Agregar al carro</button>
       </div>
     </div>
   );
